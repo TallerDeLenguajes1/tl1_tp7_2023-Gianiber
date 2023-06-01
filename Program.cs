29171e1d4aaf4791acfb5e503dc5e3745346acef
Gianfranco Bernachi <gianfrancobernachi@gmail.com>
2023-06-01 19:39:12 -0300

    Ejercicio1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
 ﻿using EspacioCalculadora;
 
 Calculadora dato = new Calculadora();
-dato.Sumar(5);
-Console.WriteLine(dato.Resultado);
-dato.Restar(2);
-Console.WriteLine(dato.Resultado);
-dato.Multiplicar(6);
-Console.WriteLine(dato.Resultado);
-dato.Dividir(9);
-Console.WriteLine(dato.Resultado);
-dato.Limpiar();
-Console.WriteLine(dato.Resultado);
-//hola
+Console.WriteLine("¿Que operación desea realizar?");
+Console.WriteLine("0-Sumar \n1-Restar \n2-Multiplicar \n3-Dividir \n4-Limpiar \n5-Salir");
+int opc = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un número");
+double valor = double.Parse(Console.ReadLine());
+while (opc != 5)
+{
+    switch (opc)
+    {
+        case 0:
+            dato.Sumar(valor);
+            break;
+        case 1:
+            dato.Restar(valor);
+            break;
+        case 2:
+            dato.Multiplicar(valor);
+            break;
+        case 3:
+            dato.Dividir(valor);
+            break;
+        case 4:
+            dato.Limpiar();
+            break;
+        default:
+            break;
+    }
+    Console.WriteLine("El resultado es: " + dato.Resultado);
+    Console.WriteLine("¿Que operación desea realizar?\n");
+    Console.WriteLine("0-Sumar \n1-Restar \n2-Multiplicar \n3-Dividir \n4-Limpiar \n5-Salir");
+    opc = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese un número");
+    valor = double.Parse(Console.ReadLine());
+}
